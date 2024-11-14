87ecda73a4d95c946dbe7a32f7876777fba51c9c
Marc <marctotaro94@gmail.com>
2024-11-14 12:05:46 -0600

    feat: Mongoose Models ready

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +10,41 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      dbName: process.env.MONGO_DB_NAME
+    });
 
+// ----- Schemas ------
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: String
+});
 
+const userSchema = new mongoose.Schema({
+  username: String
+});
+
+const logSchema = new mongoose.Schema({
+  username: String,
+  count: Number,
+  log: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
+});
+
+// ----- Models -----
+let Exercise = mongoose.model('Exercise', exerciseSchema);
+let User = mongoose.model('User', userSchema);
+let Log = mongoose.model('Log', logSchema);
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
