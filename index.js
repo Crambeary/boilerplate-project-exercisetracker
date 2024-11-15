8a38ff2c4d9903a64a2c4bb0ecd14117eb54aa47
Marc <marctotaro94@gmail.com>
2024-11-14 18:59:40 -0600

    feat: create users and find full object

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
+let bodyParser = require('body-parser');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -31,23 +32,64 @@ const userSchema = new mongoose.Schema({
 let User = mongoose.model('User', userSchema);
 
 // ----- DB Interface -----
-// Per endpoint?
+// Per endpoint
 // -- users --
 // Create Username
+const createUser = (username, done) => {
+    User.create(
+        {
+            "username": username,
+            "count": 0,
+            "log": []
+        },
+        function (err, data) {
+            done(err, data);
+        }
+    );
+}
 // Find users
+const findUsers = (done) => {
+    User.find((err, data) => {
+        console.log('data', data);
+        let filteredUsers = data.map(({count, log, ...rest}) => rest);
+        done(err, filteredUsers);
+    });
+}
 
 // -- exercise --
 // Find exercise from user
+// Create exercise appended in user log and increment count of user
 
 // -- logs --
-// Find logs from user
+// Find logs from user and return the full object
+
+
 
 // ------ API Endpoints -----
-// Post '/api/users'
-    // Return object with `username` and `_id`
-// Get '/api/users' returns list of all users in an array of objects containing `username` and `_id`
+app.use(bodyParser.urlencoded({extended: false}));
+app.route('/api/users/')
+    // Get '/api/users' returns list of all users in an array of objects containing `username` and `_id`
+    .get((req, res) => {
+        findUsers((err, data) => {
+            res.json(data);
+        })
+    })
+    // Post '/api/users'
+        // Return object with `username` and `_id`
+    .post((req, res) => {
+    console.log(req.body);
+    createUser(req.body.username, (err, data) => {
+       console.log(err, data);
+       res.json({"username": data.username, "_id": data._id});
+    });
+});
+
 // Post '/api/users/:id/exercises'
 // GET '/api/users/:id/logs'
+    // Show the full object json
+    // 16. You can add from, to and limit parameters
+    // TODO: Research GET parameters
+    // GET /api/users/:_id/logs?[from][&to][&limit]
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
